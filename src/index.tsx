906187aef87cb07d0874d935b54d66dcfcd7aed6
x_qi <x_qi@trip.com>
2023-04-10 20:22:00 +0800

    feat: config store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'; 
 
+import App from './App';
 import 'normalize.css'
 import '@/assets/css/index.less'
+import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
