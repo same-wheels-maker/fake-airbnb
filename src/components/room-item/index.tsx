fa539142efa70e1f9af0ebea398c105bc0449a61
x_qi <x_qi@trip.com>
2023-05-15 16:23:13 +0800

    feat: room item

--- src/components/room-item/index.tsx
+++ src/components/room-item/index.tsx
@@ -5,8 +5,17 @@ const RoomItem = memo((props: any) => {
   const { data } = props
 
   return (
-    <ItemWrapper>
-      <div>{data.name}</div>
+    <ItemWrapper verifyColor={data.verify_info?.text_color}>
+      <div className='inner'>
+        <div className='cover'>
+          <img src={data.picture_url} alt='' />
+        </div>
+        <div className='desc'>
+          {data.verify_info?.messages?.join?.('·')}
+        </div>
+        <div className='name'>{data.name}</div>
+        <div className='price'>￥{data.price}/晚</div>
+      </div>
     </ItemWrapper>
   ) 
 })
--- src/components/room-item/style.ts
+++ src/components/room-item/style.ts
@@ -1,8 +1,54 @@
 import styled from 'styled-components'
 
-export const ItemWrapper = styled.div`
+type WrapperProperty = {
+  verifyColor: string
+}
+
+export const ItemWrapper = styled.div<WrapperProperty>`
   box-sizing: border-box;
   width: calc((100% - 16px * 3) / 4);
   padding: 10px 8px;
   color: ${(props) => props.theme?.fontSecondaryColor};
+
+  .inner {
+    width: 100%;
+    cursor: pointer;
+
+    .cover {
+      position: relative;
+      padding: 66.66% 8px 0;
+      border-radius: 3px;
+      overflow: hidden;
+
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .desc {
+      margin: 10px 0 5px;
+      font-size: 12px;
+      font-weight: 700;
+      /* color: ${(props) => props.verifyColor}; */
+      color: ${(props) => props.theme?.lightFontPrimaryColor};
+    }
+
+    .name {
+      font-size: 16px;
+      font-weight: 600;
+      display: -webkit-box;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+
+    .price {
+      margin: 8px 0;
+    }
+  }
 `
